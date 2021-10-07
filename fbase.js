affd08e7eb7177f8ec966e8fc0f5524d8c08d92d
abrevett <37148946+abrevett@users.noreply.github.com>
2021-10-07 08:47:37 -0400

    Added basic OAuth and OTF JS pages

--- fbase.js
+++ fbase.js
@@ -1,65 +1,103 @@
-// Import the functions you need from the SDKs you need
+// Imports for appinit, rtdb and fbauth
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
 import * as rtdb from "https://www.gstatic.com/firebasejs/9.0.2/firebase-database.js";
-// TODO: Add SDKs for Firebase products that you want to use
+import * as fbauth from "https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js";
 // https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDX0kms6HZKcel43loGXmAQwmRKkvqljv0",
-    authDomain: "erisdb-6f4be.firebaseapp.com",
-    databaseURL: "https://erisdb-6f4be-default-rtdb.firebaseio.com",
-    projectId: "erisdb-6f4be",
-    storageBucket: "erisdb-6f4be.appspot.com",
-    messagingSenderId: "724347705969",
-    appId: "1:724347705969:web:be56f6a9254aef5eea19d0"
-};
+// Eris's Firebase config
+const firebaseConfig = {apiKey: "AIzaSyDX0kms6HZKcel43loGXmAQwmRKkvqljv0",authDomain: "erisdb-6f4be.firebaseapp.com",databaseURL: "https://erisdb-6f4be-default-rtdb.firebaseio.com",projectId: "erisdb-6f4be",storageBucket: "erisdb-6f4be.appspot.com",messagingSenderId: "724347705969",appId: "1:724347705969:web:be56f6a9254aef5eea19d0"};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 let db = rtdb.getDatabase(app);
+const auth = fbauth.getAuth(app);
 
 // Get Current Channel's Chat Messages
-const channelID=0; const userID="guest";
-const channelRef = rtdb.ref(db, "/channels/"+channelID+"/");
-const messageRef = rtdb.ref(db, "/channels/"+channelID+"/messages/");
-rtdb.onValue(messageRef, ss=>{
-	renderChats(ss);
-});
-$(function(){
-	$("#submit").click( function(){
-		let msg = document.querySelector("#msg").value;
-		pushMessage(msg);
-	});
-});
+let channelID=0; let userID="guest";
+let channelRef = rtdb.ref(db, "/channels/"+channelID+"/");
+let messageRef = rtdb.ref(db, "/channels/"+channelID+"/messages/");
 
-let renderChats = function(chat){
-    chat = JSON.parse(JSON.stringify(chat));
-    $("#chatbox").empty();
-    /*
-	$("#chatbox").append(
-		`<div class=msg>${JSON.parse(JSON.stringify(chat)).motd}</div>`
-	);
-	*/
-    console.log(JSON.stringify(chat));
-    let ids = Object.keys(chat);
-    console.log(JSON.stringify(ids));
-    ids.map( id => {
-	let msg = chat[id];
-	console.log(msg);
-	let chatid = JSON.stringify(id);
-	let user = msg.user;
-	let message = msg.message;
-	console.log(user + "," + message);
-	$("#chatbox").append(
-		`<div id=${chatid} class=chatitem>
-		    <div class=userid>${user}</div>
-		    <div class=msgtxt>${message}</div>
-		</div>`
-	);
+// Set the callbacks for the page loading
+let LoadLoginPage = function(){
+    document.querySelector("body").innerHTML = LoginPageT;
+    document.querySelector("#login-submit").addEventListener("click", function(){
+    //Firebase Auth Login
+    let password = document.querySelector("#login-passwd").value;
+    let email = document.querySelector("#login-email").value;
+    fbauth.signInWithEmailAndPassword(auth, email, password)
+        .then( (userCred) => {
+	    let user = userCred.user;
+	    channelID=0; 
+	    rtdb.get( rtdb.ref(db, "/users/"+user.uid) )
+		.then( (data) => {
+		    userID = data.val();
+		})
+		.catch( (err) =>{
+		    console.log(err.code);
+		    console.log(err.message);
+		});
+	LoadChatPage(channelID);
+	})
+	.catch( (err) => {
+	    console.log(err.code);
+	    console.log(err.message);
+	});
+    });
+    document.querySelector("#signup-submit").addEventListener("click", function(){
+	// Firebase Auth Register
+	if( document.querySelector("#register-passwd").value ==
+	    document.querySelector("#register-passwd2").value){
+	    let password = document.querySelector("#register-passwd").value;
+	    let email = document.querySelector("#register-email").value;
+	    fbauth.createUserWithEmailAndPassword(auth, email, password)
+		.then( (userCred) => {
+		    let user = userCred.user;
+		    console.log(userCred);
+		    console.log(user);
+		    rtdb.set( 
+			rtdb.ref(db, "/users/"+user.uid),
+			document.querySelector("#register-user").value
+		    );
+		})
+		.catch( (err) => {
+		    console.log(err.code);
+		    console.log(err.message);
+		});
+	}
+	else{
+	    alert("Passwords do not match!!!");
+	    document.querySelector("#register-passwd").value = "";
+	    document.querySelector("#register-passwd2").value = "";
+	}
+	//alert("Not functioning yet");
     });
 }
 
-let pushMessage = function(msg){
-    let pushObj = JSON.parse('{\"user\": \"guest\", \"message\": \"' + msg + '\"}');
+let LoadChatPage = function(chatid, user){
+    document.querySelector("body").innerHTML = ChatPageT;
+    messageRef = rtdb.ref(db, "/channels/"+channelID+"/messages/");
+    userID = user.uid;
+    rtdb.onValue(messageRef, msgs=>{
+	renderChats(msgs);
+    });
+    document.querySelector("#chat-submit").addEventListener("click", function(){
+	let msg = document.querySelector("#chat-message").value;
+	pushMessage(msg, userID);
+	document.querySelector("#chat-message").value = "";
+    });
+}
+
+fbauth.onAuthStateChanged(auth, (user) =>{
+    if(!!user){
+	// User is logged in, go directly to Chat Page
+	LoadChatPage(channelID, user);
+    }
+    else{
+	// Send to Login Page
+	LoadLoginPage();
+    }
+});
+
+let pushMessage = function(msg, id){
+    let pushObj = JSON.parse('{\"user\": \"' + id + '\", \"message\": \"' + msg + '\"}');
     rtdb.push(messageRef, pushObj);
 }
